47eb279fa6c13681cd7f7b2af59d87b8f98fbe52
Chris <cdlight@ucsd.edu>
2021-07-19 19:07:33 -0700

    Jump functionality implemented

--- Assets/CombatH&S/Scripts/PlayerController.cs
+++ Assets/CombatH&S/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -17,11 +19,20 @@ public class PlayerController : MonoBehaviour
     private float movementSpeedModifier = 2f;
     private float rotationSpeed = 215f;
 
+    // jump vars
+    public float jumpForce = 5.0f;
+    public float jumpImpulse = .5f;
+    public float gravity = -9.8f;
+    private float speedVert;
+    private bool isGrounded;
+
     private Animator[] animators;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        speedVert = 0.0f; //player starts at rest
+        isGrounded = true;
     }
 
     private void Awake()
@@ -44,8 +55,6 @@ public class PlayerController : MonoBehaviour
 
         aim = cubeInputActions.Player.Aim;
         aim.Enable();
-//        cubeInputActions.Player.Run.performed += DoRun;
-//        cubeInputActions.Player.Run.Enable();
     }
 
     private void OnDisable()
@@ -54,13 +63,7 @@ public class PlayerController : MonoBehaviour
         run.Disable();
         attack.Disable();
         aim.Disable();
-//        cubeInputActions.Player.Run.Disable();
     }
-
-    /*    private void DoRun(InputAction.CallbackContext obj)
-        {
-            //movementSpeed = movementSpeed * movementSpeedModifier;
-        }*/
 
     private void Update()
     {
@@ -71,126 +74,11 @@ public class PlayerController : MonoBehaviour
             animators[1].Play("swingHammerAnimation");
         }
 
-        movePlayerTo();
+        MovePlayerTo();
     }
 
-    // Old player position x & z transform movement:
-    private void oldMovePlayerH(Vector3 movementVector)
+    private void MovePlayerTo()
     {
-        bool running = Mathf.Approximately(run.ReadValue<float>(), 1);
-
-        if (movementVector != Vector3.zero && !running)
-        {
-            controller.Move(movementVector * Time.deltaTime * 3.14f * movementSpeed);
-
-            Quaternion toRotation = Quaternion.LookRotation(movementVector, Vector3.up);
-            controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-            //animator.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
-            animators[0].SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
-
-        }
-        else if (movementVector != Vector3.zero && running)
-        {
-            if (StaminaBar.instance.IsStaminaEmpty())
-            {
-                controller.Move(movementVector * Time.deltaTime * 3.14f * movementSpeed);
-
-                Quaternion toRotation = Quaternion.LookRotation(movementVector, Vector3.up);
-                controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-                animators[0].SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
-            }
-            else
-            {
-                StaminaBar.instance.UseStamina(0.1f);
-                controller.Move(movementVector * Time.deltaTime * 3.14f * movementSpeed * 2f);
-
-                Quaternion toRotation = Quaternion.LookRotation(movementVector, Vector3.up);
-                controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-                animators[0].SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
-            }
-        }
-        else
-        {
-            controller.Move(movementVector * Time.deltaTime * 3.14f * movementSpeed);
-            // animator.SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
-            animators[0].SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
-
-        }
-    }
-
-    private void movePlayer()
-    {
-
-        var cameraForwardDirection = Camera.main.transform.forward;
-        Debug.DrawRay(Camera.main.transform.position, cameraForwardDirection * 10, Color.red);
-        var cameraVector = Vector3.Scale(cameraForwardDirection, (Vector3.right + Vector3.forward));
-        Debug.DrawRay(Camera.main.transform.position, cameraVector * 10, Color.blue);
-
-        Vector2 inputVector = movement.ReadValue<Vector2>();
-        Vector3 movementVector = new Vector3();
-
-        movementVector.x = inputVector.x;
-        movementVector.z = inputVector.y;
-
-        bool aiming = Mathf.Approximately(aim.ReadValue<float>(), 1);
-        bool running = Mathf.Approximately(run.ReadValue<float>(), 1);
-
-        Vector3 camF = Camera.main.transform.forward;
-        Vector3 camR = Camera.main.transform.right;
-
-        camF.y = 0;
-        camR.y = 0;
-
-        camF = camF.normalized;
-        camR = camR.normalized;
-
-        Vector3 newPlayerMovementVector = camF * movementVector.z + camR * movementVector.x;
-
-        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.right * 10, Color.yellow);
-
-
-        if (movementVector != Vector3.zero && !running) // Walking
-        {
-
-            controller.Move(newPlayerMovementVector * 0.5f * Time.deltaTime * 3.14f * movementSpeed);
-            Quaternion toRotation = Quaternion.LookRotation(newPlayerMovementVector, Vector3.up);
-            controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-
-        }
-        else if (movementVector != Vector3.zero && running) // Running
-        {
-            if (StaminaBar.instance.IsStaminaEmpty())
-            {
-                controller.Move(newPlayerMovementVector * 0.5f * Time.deltaTime * 3.14f * movementSpeed);
-                Quaternion toRotation = Quaternion.LookRotation(newPlayerMovementVector, Vector3.up);
-                controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-                animators[0].SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
-            }
-            else
-            {
-                StaminaBar.instance.UseStamina(0.05f);
-                controller.Move(newPlayerMovementVector * 0.5f * Time.deltaTime * 3.14f * movementSpeed * movementSpeedModifier);
-                Quaternion toRotation = Quaternion.LookRotation(newPlayerMovementVector, Vector3.up);
-                controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-                animators[0].SetFloat("Speed", 1f, 0.1f, Time.deltaTime);
-            }
-        }
-        else // Idle
-        {
-            if (aiming)
-            {
-                Quaternion toRotation = Quaternion.LookRotation(cameraVector, Vector3.up);
-                controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-            }
-
-            animators[0].SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
-        }
-
-    }
-
-    private void movePlayerTo()
-    {
-
         var cameraForwardDirection = Camera.main.transform.forward;
         var cameraVector = Vector3.Scale(cameraForwardDirection, (Vector3.right + Vector3.forward));
 
@@ -213,36 +101,56 @@ public class PlayerController : MonoBehaviour
         camF = camF.normalized;
         camR = camR.normalized;
 
-        Vector3 newPlayerMovementVector = camF * movementVector.z + camR * movementVector.x;
+        Vector3 newPlayerMovementVector;
+        newPlayerMovementVector = camF * movementVector.z + camR * movementVector.x;
 
         if (moving && !running) // Walking
         {
-            playerRotation(aiming, cameraVector, newPlayerMovementVector);
-            controller.Move(newPlayerMovementVector * 0.5f * Time.deltaTime * 3.14f * movementSpeed);
+            PlayerRotation(aiming, cameraVector, newPlayerMovementVector);
+            controller.Move(newPlayerMovementVector * (0.5f * Time.deltaTime * 3.14f * movementSpeed) + PlayerTranslation(canRun));
+            animators[0].SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
             animators[0].SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
         }
         else if (moving && running && canRun) // Running
         {
-            playerRotation(aiming, cameraVector, newPlayerMovementVector);
+            PlayerRotation(aiming, cameraVector, newPlayerMovementVector);
             StaminaBar.instance.UseStamina(0.05f);
-            controller.Move(newPlayerMovementVector * 0.5f * Time.deltaTime * 3.14f * movementSpeed * movementSpeedModifier);
+            controller.Move(newPlayerMovementVector * (0.5f * Time.deltaTime * 3.14f * movementSpeed * movementSpeedModifier) + PlayerTranslation(canRun));
             animators[0].SetFloat("Speed", 1f, 0.1f, Time.deltaTime);
         }
         else if (moving && running && !canRun) // Attempting to run
         {
-            playerRotation(aiming, cameraVector, newPlayerMovementVector);
-            controller.Move(newPlayerMovementVector * 0.5f * Time.deltaTime * 3.14f * movementSpeed);
+            PlayerRotation(aiming, cameraVector, newPlayerMovementVector);
+            controller.Move(newPlayerMovementVector * (0.5f * Time.deltaTime * 3.14f * movementSpeed) + PlayerTranslation(canRun));
             animators[0].SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
         }
         else // Idle
         {
+            controller.Move(PlayerTranslation(canRun));
+            
             if (aiming)
-                playerRotation(aiming, cameraVector, newPlayerMovementVector);
+                PlayerRotation(aiming, cameraVector, newPlayerMovementVector);
             animators[0].SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
         }
     }
 
-    private void playerRotation(bool aiming, Vector3 cameraVector, Vector3 movementVector)
+    private Vector3 PlayerTranslation(bool canRun) //modify vertical motions
+    {
+        isGrounded = (controller.collisionFlags & CollisionFlags.Below) != 0;
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && canRun) //jump
+        {
+            StaminaBar.instance.UseStamina(5.0f);
+            speedVert = jumpForce;
+            return transform.up * jumpImpulse;
+        }
+        else //falling
+        {
+            speedVert += gravity * Time.deltaTime;
+            return transform.up * (speedVert * Time.deltaTime);
+        }
+    }
+    
+    private void PlayerRotation(bool aiming, Vector3 cameraVector, Vector3 movementVector)
     {
         Quaternion toRotation = aiming ? Quaternion.LookRotation(cameraVector, Vector3.up) : Quaternion.LookRotation(movementVector, Vector3.up); 
         controller.transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
