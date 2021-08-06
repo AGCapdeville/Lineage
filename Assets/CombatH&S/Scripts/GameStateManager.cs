ea41206626add156d99a9bc60be249cf867554af
Adam <agcapdeville@gmail.com>
2021-08-06 12:52:24 -0700

    Starting Singleton

---
+++ Assets/CombatH&S/Scripts/GameStateManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance { get; private set; }
+    
+}
